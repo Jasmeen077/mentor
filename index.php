3575b072551f6403cc14d93e6622d5495b107eff
jasmeen077 <jasmeen.khanam@idslogic.com>
2026-03-26 11:09:19 +0530

    update index.php code

--- index.php
+++ index.php
@@ -16,34 +16,40 @@ $PAGE->set_pagelayout('standard');
 $PAGE->requires->css('/local/mentor/styles.css');
 
 $mentors = \local_mentor\mentor_queries::get_mentors($USER->id);
+
 $fieldid = $DB->get_field('user_info_field', 'id', ['shortname' => 'ratings']);
 
-if ($fieldid) {
+foreach ($mentors as $mentor) {
 
-  $existing = $DB->get_record('user_info_data', [
-    'userid' => $mentor->id,
-    'fieldid' => $fieldid
-  ]);
+    $mentor->bio = strip_tags($mentor->bio ?? '');
+    $mentor->expertise = strip_tags($mentor->expertise ?? '');
 
-  $profiledata = new stdClass();
-  $profiledata->userid = $mentor->id;
-  $profiledata->fieldid = $fieldid;
-  $profiledata->data = $mentor->averagerating ?? 0;
+    $rating = round($mentor->averagerating ?? 0);
+    $mentor->stars = str_repeat('★', $rating) . str_repeat('☆', 5 - $rating);
 
-  if ($existing) {
-    $profiledata->id = $existing->id;
-    $DB->update_record('user_info_data', $profiledata);
-  } else {
-    $DB->insert_record('user_info_data', $profiledata);
-  }
+    if ($fieldid) {
+
+        $existing = $DB->get_record('user_info_data', [
+            'userid' => $mentor->id,
+            'fieldid' => $fieldid
+        ]);
+
+        $profiledata = new stdClass();
+        $profiledata->userid = $mentor->id;
+        $profiledata->fieldid = $fieldid;
+        $profiledata->data = $mentor->averagerating ?? 0;
+
+        if ($existing) {
+            $profiledata->id = $existing->id;
+            $DB->update_record('user_info_data', $profiledata);
+        } else {
+            $DB->insert_record('user_info_data', $profiledata);
+        }
+    }
 }
 
-// Renderer
 $output = $PAGE->get_renderer('local_mentor');
 
-// Page output
 echo $OUTPUT->header();
-// Mentor cards
 echo $output->render_mentor_cards($mentors);
-
 echo $OUTPUT->footer();
