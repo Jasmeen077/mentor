026e468fcbfd5535fa50e6357a20c2200134f3c1
jasmeen077 <jasmeen.khanam@idslogic.com>
2026-04-07 12:08:07 +0530

    updated quiz and assignment notification

--- db/events.php
+++ db/events.php
@@ -11,4 +11,20 @@ $observers = [
         'eventname' => '\core\event\user_enrolment_updated',
         'callback' => '\local_mentor\observer::user_enrolled',
     ],
+    // [
+    //     'eventname' => '\core\event\user_enrolment_created',
+    //     'callback' => '\local_mentor\observer::user_email',
+    // ],
+    [
+        'eventname' => '\mod_quiz\event\attempt_submitted',
+        'callback' => 'local_mentor\observer::quiz_submit_mail',
+    ],
+    [
+        'eventname' => '\mod_assign\event\assessable_submitted',
+        'callback' => 'local_mentor\observer::assignment_submit_mail',
+    ],
+    [
+        'eventname' => '\core\event\role_assigned',
+        'callback' => '\local_mentor\observer::user_email',
+    ],
 ];
